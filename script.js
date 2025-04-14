0657587cafa0e3ae6c7ac1ce18d9a1b58cb2e693
Elena-92 <missy.vip@bk.ru>
2025-04-14 21:07:25 +0500

    Задание 8

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let num = 1;
-alert(num += 5);//6
-alert(num -= 3);//3
-alert(num *= 7);//21
-alert(num /= 3);//7
-alert(num ++);//8
-alert(num --);//7
-alert(num);
+let age = prompt(`Сколько вам лет?`);
+alert(age);
+
