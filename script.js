402dd6a59fa52e9843ab65a64f67279e94652623
Elena-92 <missy.vip@bk.ru>
2025-04-14 21:05:06 +0500

    Задание 7

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let a = 9;
-let b = 2;
-result = a / b;
-alert(result);
+let num = 1;
+alert(num += 5);//6
+alert(num -= 3);//3
+alert(num *= 7);//21
+alert(num /= 3);//7
+alert(num ++);//8
+alert(num --);//7
+alert(num);
