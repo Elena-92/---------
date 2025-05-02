2475b8bc65f5ea6e5b64b0ddf8f3524b1fa92967
Elena-92 <missy.vip@bk.ru>
2025-05-02 11:20:25 +0500

    Д/З № 3 Задание 1.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let newName = String(prompt(`Как ваше имя?`));
-alert(`Привет, ${newName}!`);
+let password = String('пароль' );
+let password2 = String('кошка');
+let answer = String(prompt('Введите пароль'));
+answer = answer.toLocaleLowerCase();
+if (answer === 'кошка') {alert('Пароль введен верно');
+  
+} else { alert('Пароль введен неправильно')
+  
+};
