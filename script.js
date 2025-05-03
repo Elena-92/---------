080e0777d07ebfae31d0b32567f8583aec89c6fa
Elena-92 <missy.vip@bk.ru>
2025-05-03 17:21:07 +0500

    Д/З №3 задание 5

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-let a = '2';
-let b = '3';
-// Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
-alert (parseInt(a) + parseInt(b));
+let monthNumber = Number(prompt('Введите месяц'));
+
+switch (monthNumber) {
+  case 1:
+    alert('зима');
+    break;
+  case 2:
+    alert('зима');
+    break;
+  case 3:
+    alert('весна');
+    break;
+  case 4:
+    alert('весна');
+    break;
+  case 5:
+    alert('весна');
+  break;  
+  case 6:
+    alert('лето');
+    break;
+  case 7:
+    alert('лето');
+    break
+  case 8:
+    alert('лето');
+  break;  
+  case 9:
+    alert('осень');
+    break;
+  case 10:
+    alert('осень');
+    break;
+  case 11:
+    alert('осень');
+    break;
+    case 12:
+    alert('зима');
+    break;
+default: alert('Такого месяца не существует!');
+    break;
+
+}
+
+
