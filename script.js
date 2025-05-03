1d853d6e5cce13d2afe14e81d3a55e8f21ad5c5d
Elena-92 <missy.vip@bk.ru>
2025-05-03 18:39:27 +0500

    Д/З № 3 дополнит задание

--- script.js
+++ script.js
@@ -1,45 +1,32 @@
-let monthNumber = Number(prompt('Введите месяц'));
+const value = prompt('Пожалуйста, введите любое число');
+const result = Number(value);
+if (isNaN(result)) {
+  alert('Нечисло');
+  
+} else {
+ alert('Число');
+  
+}
 
-switch (monthNumber) {
-  case 1:
-    alert('зима');
-    break;
-  case 2:
-    alert('зима');
-    break;
-  case 3:
-    alert('весна');
-    break;
-  case 4:
-    alert('весна');
-    break;
-  case 5:
-    alert('весна');
-  break;  
-  case 6:
-    alert('лето');
-    break;
-  case 7:
-    alert('лето');
-    break
-  case 8:
-    alert('лето');
-  break;  
-  case 9:
-    alert('осень');
-    break;
-  case 10:
-    alert('осень');
-    break;
-  case 11:
-    alert('осень');
-    break;
-    case 12:
-    alert('зима');
-    break;
-default: alert('Такого месяца не существует!');
-    break;
 
+const value1 = prompt('Пожалуйста, введите любое число');
+const result1 = Number(value1);
+if(result1 % 2 === 0 ) {
+  alert('Чётное число');
+  
+} else {
+ alert('Нечётное число');
+  
+}
+
+
+let clientOS = Number(prompt('Какая у вас OS,где 0 - это iOS, а 1 - это Android?'));
+if (clientOS === 0) {
+  alert('Установите версию приложения для iOS по ссылке');
+  
+} else {
+  alert('Установите версию приложения для Android по ссылке');
+  
 }
 
 
