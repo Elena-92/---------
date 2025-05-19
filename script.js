39293cd1ae8441de9c47ee9b3942fcb1eda686dd
Elena-92 <missy.vip@bk.ru>
2025-05-19 22:20:28 +0500

    HW 3

--- script.js
+++ script.js
@@ -1,32 +1,77 @@
-const value = prompt('Пожалуйста, введите любое число');
-const result = Number(value);
-if (isNaN(result)) {
-  alert('Нечисло');
-  
-} else {
- alert('Число');
-  
-}
+let password = String('пароль' );
+let password2 = String('кошка');
+let answer = String(prompt('Введите пароль'));
+answer = answer.toLocaleLowerCase();
+if (answer === 'кошка') {alert('Пароль введен верно');
+
+} else { alert('Пароль введен неправильно')
+
+};
 
 
-const value1 = prompt('Пожалуйста, введите любое число');
-const result1 = Number(value1);
-if(result1 % 2 === 0 ) {
-  alert('Чётное число');
-  
-} else {
- alert('Нечётное число');
-  
-}
+let c = Number('0');
+let d = Number('10');
+
+if (c>0 && c<10) {alert('Верно');
+};
 
 
-let clientOS = Number(prompt('Какая у вас OS,где 0 - это iOS, а 1 - это Android?'));
-if (clientOS === 0) {
-  alert('Установите версию приложения для iOS по ссылке');
-  
-} else {
-  alert('Установите версию приложения для Android по ссылке');
-  
-}
 
 
+    let dd = Number('59');
+    let e = Number('188');
+    if (d>100 || e>100) {alert('Верно');
+    
+    } else { alert('Неверно')
+    };
+
+
+    let a = '2';
+    let b = '3';
+    // Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+    alert (parseInt(a) + parseInt(b));
+
+
+    let monthNumber = Number(prompt('Введите месяц'));
+
+    switch (monthNumber) {
+    case 1:
+    alert('зима');
+    break;
+    case 2:
+    alert('зима');
+    break;
+    case 3:
+    alert('весна');
+    break;
+    case 4:
+    alert('весна');
+    break;
+    case 5:
+    alert('весна');
+    break;  
+    case 6:
+    alert('лето');
+    break;
+    case 7:
+    alert('лето');
+    break
+    case 8:
+    alert('лето');
+    break;  
+    case 9:
+    alert('осень');
+    break;
+    case 10:
+    alert('осень');
+    break;
+    case 11:
+    alert('осень');
+    break;
+    case 12:
+    alert('зима');
+    break;
+    default: alert('Такого месяца не существует!');
+    break;
+
+};
