f55d436d4895c6d8ffe328e247f33f8c2c3ff974
Elena-92 <missy.vip@bk.ru>
2025-10-01 20:35:24 +0500

    редактированы игры переверни текст и викторина

--- script.js
+++ script.js
@@ -1,149 +1,94 @@
-
-
 //Task#1
-const myArray = [1, 5, 4, 10, 0, 3];
-for (let index = 0; index < myArray.length; index++) {
-    alert(myArray[index]);
-    
-    if (myArray[index] === 10) {
-        break;
-    }
-};
+let text = "js";
+let textAltered = text.toUpperCase();
+alert(textAltered);
 
 //Task#2
+const searchElement = (elements) => {
+    const newArray = new Array();
+    const findIt = 'арбуз';
+    elements.forEach(element => {
+        if (element.toLowerCase().startsWith(findIt.toLowerCase())) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
+alert(searchElement(['Астраханский арбуз',  'ВОЛжанин арбуз','Томаты', 'Принц Гамлет арбуз', 'арбуз Лунный', 'Арбузные дольки', 'груша', 'Яблоко Голден']));
 
-const array = Array(1, 5, 4, 10, 0, 3);
-alert(array);
-let numberValue = array.indexOf(4);
-alert(`Индекс значения "четыре" в массиве: ${numberValue}`);
 
- //Task#3
-
- const newArr= [1, 3, 5, 10, 20];
-alert(newArr.join(' '));
-
-
+//Task#3
+const num = 32.58884;
+alert(`До меньшего целого ${Math.floor(num)}`);
+alert(`До большего целого ${Math.ceil(num)}`);
+alert(`До ближайшего целого ${Math.round(num)}`);
 
 //Task#4
-let arr = [];
+const numbers = [52, 53, 49, 77, 21, 32];
+let number = 0;
 
-for (let index = 0; index < 3; index++) {
-   let insertArr = [];
-  
-        for (let i = 0; i < 3; i++) {
-            insertArr.push(1);
-        }
-        
-  arr.push(insertArr);      
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] > number) {
+        number = numbers[index];
+    }
+
 }
+alert(`Максимальное значение равно: ${number}`);
 
-alert(arr);
 
+const toFindMin = () => {
+    return Math.min(52, 53, 49, 77, 21, 32);
+}
+alert(`Минимальное значение равно: ${toFindMin()}`);
 
 //Task#5
 
-const anotherArray = [1, 1, 1];
-anotherArray.push(2, 2, 2);
-alert(anotherArray);
+const randomNumber = () => {
+    return Math.floor(Math.random() * 10 + 1);
+}
+alert(randomNumber());
 
 //Task#6
 
-const toSortArray = [9, 8, 7, 'a', 6, 5];
-toSortArray.sort();
-let sortedArray = toSortArray.sort((a, b) => b - a);
-let lastElement = toSortArray.pop();
-alert(sortedArray);
+function randomArray(n) {
+    newArray = new Array();
+    let randomNum = Math.floor(Math.random() * n);
+    const newArrayLenth = n / 2;
+    for (let i = 0; i < newArrayLenth; i++) {
+        newArray.push(randomNum);
+
+    }
+    return newArray;
+}
+alert(randomArray(6));
  
 //Task#7
-const numberArray = [9, 8, 7, 6, 5];
-let userAnswer = Number(prompt("Введите число!"));
-if (numberArray.includes(userAnswer)) {
-    alert("Угадал");
-} else {
-    alert("Не угадал");
-}
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+  }
+  alert(getRandomInt(3, 8));
 
 //Task#8
-const text = 'abcdef';
-const splitText = text.split('');
-alert(splitText);
-alert(splitText.reverse().join(''));
+let todaysDate = new Date();
+alert(todaysDate);
+
 
 //Task#9
-const numberArr = [[1, 2, 3],[4, 5, 6]];
-const simpleArr = numberArr.flat();
-alert(simpleArr);
-
+let myDate = new Date();
+let date_1 = myDate.getDate();
+let date_2 = myDate.setDate(73);
+let date1Calculated = date_1 * 24 * 3600 * 1000;
+let result = new Date(date1Calculated + date_2);
+alert(result);
 //Task#10
 
-const randomArray = [];
-let randomNum;
-for (let a = 0; a < 10; a++) {
-   randomNum = Math.floor(Math.random() * 10 + 1);
-   randomArray.push(randomNum);
-        
-} 
-alert(randomArray);
-
-for (let j = 0; j < randomArray.length - 1; j++) {
-    alert(randomArray[j] + randomArray[j + 1]);
-}
-
-//Task#11
-const squredNum = () => {
-    const newArr = new Array();
-
-    for (let h = 0; h < 5; h++) {
-    let newNum = Math.floor(Math.random() * 10 + 1);
-    newArr.push(newNum);      
-    } 
-
-    alert(newArr);
-
-    return newArr.map(item => item ** 2);
-}
-alert(squredNum());
-
-//Task#12
-const getTextLenth = (textArray) => textArray.map(item => item.length)
-
-const result = getTextLenth(["Вася", "Иннокентий", "Евгений", "Елена"]);
-alert(result);
-
-//Task#13
-
-const getNumber = (numberArray) => {
-    return numberArray.filter(number => number < 0);
-}
-alert(getNumber([5, 4, -8, 5, 11, -56,  4, -1, 6, -22, 9]));
-
-//Task#14
-
-const randomArr = [];
-        const newNumArr = [];       
-        for (let m = 0; m < 10; m++) {
-           randomArr.push(Math.floor(Math.random() * 10));
-                if (randomArr[m] % 2 === 0) {
-                        newNumArr.push(randomArr[m]);
-                }
-        } 
-        alert(randomArr);
-        alert(newNumArr);
-
-        //Task#15
-
-        function myRandomNum() {
-            return Math.floor(Math.random() * 10 + 1);
-            
-        }
-        
-        const newRandomArr = [];     
-
-        for (let q = 0; q < 6; q++) {
-          newRandomArr.push(myRandomNum());
-        }
-
-        alert(newRandomArr);
-        const sum = newRandomArr.reduce((addition, number) => addition + number, 0);
-        alert(`Среднее арифметическое равно: ${Math.floor(sum / newRandomArr.length)}`);
+const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+const days = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+let newDate = new Date();
+let fullDate = "Дата: " + newDate.getDate() + " " + months[newDate.getMonth()] + " " + newDate.getFullYear() + " год " + " - это " + days[newDate.getDay()];
+let fullTime = "Время: " + newDate.toLocaleTimeString('ru-RU');
+alert(fullDate);
+alert(fullTime);
